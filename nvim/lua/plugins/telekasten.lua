90ce319f137ba370139572c680c680bedde88c6d
Eduardo Lucca <edlucca@student.hive.fi>
2025-08-26 11:58:34 +0300

    add telekasten plugin

---
+++ nvim/lua/plugins/telekasten.lua
@@ -0,0 +1,27 @@
+return {
+  "renerocksai/telekasten.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  event = { "BufRead", "BufNewFile" },
+  config = function()
+    local home = vim.fn.expand("~/notes") -- change to your notes dir
+
+    require('telekasten').setup({
+      home = home,
+      auto_set_filetype = true,
+      dailies = home .. "/daily",
+      templates = home .. "/templates",
+      -- add more options here if needed
+    })
+
+    local opts = { noremap = true, silent = true }
+
+    vim.api.nvim_set_keymap('n', '<leader>zn', "<cmd>lua require('telekasten').new_note()<CR>", opts)
+    vim.api.nvim_set_keymap('n', '<leader>zf', "<cmd>lua require('telekasten').find_notes()<CR>", opts)
+    vim.api.nvim_set_keymap('n', '<leader>zd', "<cmd>lua require('telekasten').find_daily_notes()<CR>", opts)
+    vim.api.nvim_set_keymap('n', '<leader>zg', "<cmd>lua require('telekasten').search_notes()<CR>", opts)
+    vim.api.nvim_set_keymap('n', '<leader>zb', "<cmd>lua require('telekasten').show_backlinks()<CR>", opts)
+
+    vim.api.nvim_set_keymap('n', '<Tab>', 'za', { noremap = true, silent = true })
+  end,
+}
+
