d35d415f995015396e41504507a6687c7d3acea1
Eduardo Lucca <edlucca@student.hive.fi>
2025-09-12 16:24:13 +0300

    update startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Programs to run
-program1="gnome-terminal -- tmux"
+program1="gnome-terminal -- ./tmux_sessionizer.sh"
 program2="flatpak run com.brave.Browser"
 program3="flatpak run com.discordapp.Discord"
 
-# Workspaces list
-workspace1=1
-workspace2=2
-workspace3=3
+# # Workspaces list
+# workspace1=1
+# workspace2=2
+# workspace3=3
 
 $program1 &
-sleep 2
+sleep 1
 
 $program2 &
-sleep 2
+sleep 1
 
 $program3 &
-sleep 2
+sleep 1
---
+++ scripts/tmux_sessionizer.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Set Session Name
+SESSION="42-project"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+# Only create tmux session if it doesn't already exist
+if [ "$SESSIONEXISTS" = "" ]
+then
+    # Start New Session with our name
+    tmux new-session -d -s $SESSION
+
+    # setup Writing window
+    tmux new-window -t $SESSION:0 -n 'Code'
+    tmux send-keys -t 'Code' "nvim ." C-m
+
+    # Setup an additional shell
+    tmux new-window -t $SESSION:1 -n 'Shell'
+    tmux send-keys -t 'Shell' "fish" C-m 'clear' C-m
+fi
+
+# Attach Session, on the Main window
+tmux attach-session -t $SESSION:0
