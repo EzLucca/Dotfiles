4d037a20338615dd2452ef762dfc0935df214fc0
Eduardo Lucca <edlucca@student.hive.fi>
2026-01-08 17:22:52 +0200

    Auto-sync: Thu Jan  8 05:22:52 PM EET 2026

--- scripts/specific_note.sh
+++ scripts/specific_note.sh
@@ -9,18 +9,19 @@ if [[ -z "$NAME" ]]; then
   exit 1
 fi
 
-DATE_HUMAN="$(date +'%d-%m-%Y')"
-DATE_FILE="$(date +'%m-%d')"
+DATE_UPDATE="$(date +'%Y-%m-%d-%H-%M')"
 
 NOTES_DIR="$HOME/Documents/Notes"
 mkdir -p "$NOTES_DIR"
 
-SLUG=$(echo "$NAME" | tr '[:upper:]' '[:lower:]' | tr -cs 'a-z0-9' '-')
-FILEPATH="$NOTES_DIR/$SLUG$DATE_FILE.md"
+SLUG=$(echo "$NAME" | tr '[:upper:]' '[:lower:]')
+FILEPATH="$NOTES_DIR/$SLUG.md"
 
 if [[ ! -f "$FILEPATH" ]]; then
 cat > "$FILEPATH" <<EOF
-# $NAME – $DATE_HUMAN
+last update: $DATE_UPDATE
+
+# TITLE: $NAME
 
 ## Project
 - 
