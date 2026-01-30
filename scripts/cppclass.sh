1bf56d21fe0522b323389c8bf5e1cbb989d264d8
Eduardo Lucca <edlucca@student.hive.fi>
2026-01-30 18:01:19 +0200

    Auto-sync: Fri Jan 30 06:01:19 PM EET 2026

---
+++ scripts/cppclass.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 ClassName"
+    exit 1
+fi
+
+CLASS_NAME=$1
+HEADER_FILE="${CLASS_NAME}.hpp"
+SOURCE_FILE="${CLASS_NAME}.cpp"
+INCLUDE_GUARD=$(echo "${CLASS_NAME}_HPP" | tr '[:lower:]' '[:upper:]')
+
+# Create header file
+cat <<EOF > $HEADER_FILE
+#ifndef $INCLUDE_GUARD
+#define $INCLUDE_GUARD
+
+class $CLASS_NAME {
+public:
+    $CLASS_NAME();
+    ~$CLASS_NAME();
+
+private:
+};
+
+#endif // $INCLUDE_GUARD
+EOF
+
+# Create source file
+cat <<EOF > $SOURCE_FILE
+#include "$HEADER_FILE"
+
+$CLASS_NAME::$CLASS_NAME() {
+}
+
+$CLASS_NAME::~$CLASS_NAME() {
+}
+EOF
+
+echo "Created $HEADER_FILE and $SOURCE_FILE"
