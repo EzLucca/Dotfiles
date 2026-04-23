1acdbb189d5b4c9420a9c1d5647bcafef5b488c3
edlucca <lucca.ed@gmail.com>
2026-04-23 21:50:29 +0300

    Auto-sync: Thu Apr 23 09:50:29 PM EEST 2026

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,10 @@
---
--- return {
---   {
---     "nvim-treesitter/nvim-treesitter",
---     build = ":TSUpdate",
---     config = function ()
---       local config = require("nvim-treesitter.configs")
---       config.setup({
---         auto_install = true,
---         highlight = { enable = true },
---         indent = { enable = false},
---       })
---     end
---   }
--- }
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+
+    event = { "BufReadPost", "BufNewFile" }, -- 👈 KEY CHANGE
+
     config = function ()
       local config = require("nvim-treesitter.configs")
 
@@ -32,8 +20,6 @@ return {
 
         highlight = {
           enable = true,
-
-          -- important for markdown
           additional_vim_regex_highlighting = { "markdown" },
         },
 
