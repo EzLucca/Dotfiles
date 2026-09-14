cc38a561808b1cc28f002295f0debf04b72cbed3
edlucca <lucca.ed@gmail.com>
2026-09-14 21:39:38 +0300

    Auto-sync: Mon Sep 14 09:39:38 PM EEST 2026

--- nvim/lua/personal.lua
+++ nvim/lua/personal.lua
@@ -1,23 +1,36 @@
 -- personal.lua ----------------------------------------------------------------
 
 vim.keymap.set("n", "<leader>ov", function()
-	vim.cmd("lcd %:p:h")
-	vim.cmd("vert term")
+    vim.cmd("lcd %:p:h")
+    vim.cmd("vert term")
 end, { desc = "Open vertical term" })
 
 vim.keymap.set("n", "<leader>os", function()
-	vim.cmd("lcd %:p:h")
-	vim.cmd("10sp | term")
+    vim.cmd("lcd %:p:h")
+    vim.cmd("10sp | term")
 end, { desc = "Open horizontal term" })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-	pattern = "*",
-	callback = function()
-		local opts = { buffer = 0, noremap = true }
-		-- Navigate windows from terminal
-		vim.keymap.set("t", "<leader>wh", [[<C-\><C-n><C-w>h]], opts)
-		vim.keymap.set("t", "<leader>wj", [[<C-\><C-n><C-w>j]], opts)
-		vim.keymap.set("t", "<leader>wk", [[<C-\><C-n><C-w>k]], opts)
-		vim.keymap.set("t", "<leader>wl", [[<C-\><C-n><C-w>l]], opts)
-	end,
+    pattern = "*",
+    callback = function()
+        local opts = { buffer = 0, noremap = true }
+        -- Navigate windows from terminal
+        vim.keymap.set("t", "<leader>wh", [[<C-\><C-n><C-w>h]], opts)
+        vim.keymap.set("t", "<leader>wj", [[<C-\><C-n><C-w>j]], opts)
+        vim.keymap.set("t", "<leader>wk", [[<C-\><C-n><C-w>k]], opts)
+        vim.keymap.set("t", "<leader>wl", [[<C-\><C-n><C-w>l]], opts)
+    end,
 })
+
+-- Open current file with the system's default application
+vim.keymap.set("n", "<leader>z", function()
+    local file = vim.fn.expand("<cfile>")
+    local dir = vim.fn.expand("%:p:h")
+    local path = vim.fn.fnamemodify(dir .. "/" .. file, ":p")
+
+    if vim.fn.filereadable(path) == 0
+        then vim.notify("Not a readable file: " .. path, vim.log.levels.ERROR)
+            return
+        end
+        vim.fn.jobstart({ "/usr/bin/zathura", path }, { detach = true, })
+        end, { desc = "Open file in Zathura" })
